6ac1a15dfd39e8cb79aa7586b9e96a18d009c455
MadisonViaLux <madison.s.mckown@gmail.com>
2019-05-30 15:59:15 -0700

    more slow progress

--- components/Dropdown/Dropdown.js
+++ components/Dropdown/Dropdown.js
@@ -12,16 +12,18 @@ class Dropdown {
     
     // Add a click handler to the button reference and call the toggleContent method.
     this.button.addEventListener('click', () => {
-      this.element.toggleContent();
+      this.button.toggleContent();
     })
   }
 
   toggleContent() {
     // Toggle the ".dropdown-hidden" class off and on
     this.content.classList.toggle('dropdown-hidden');
-  }
+  };
 }
 
 
 // Nothing to do here, just study what the code is doing and move on to the Dropdown class
 const dropdowns = document.querySelectorAll('.dropdown').forEach( dropdown => new Dropdown(dropdown));
+
+console.log(dropdowns);
--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -5,24 +5,24 @@ class TabLink {
     this.element = element;
     
     // Get the custom data attribute on the Link
-    this.data;
+    this.data = document.querySelector(`.tabs-link[data-tab='${element.dataset.tab}']`);
     
     // Using the custom data attribute get the associated Item element
-    // this.itemElement;
+    this.itemElement = document.querySelector('.tabs-item');
     
     // Using the Item element, create a new instance of the TabItem class
-    // this.tabItem;
+    this.tabItem = new TabItem(this.element);
     
     // Add a click event listener on this instance, calling the select method on click
-
+    this.tabItem.addEventListener('click', () => this.select());
   };
 
   select() {
     // Get all of the elements with the tabs-link class
-    // const links;
+    const links = document.querySelector('.tabs-link');
 
     // Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
-    // Array.from(links).forEach();
+    Array.from(links).forEach(content => content.classList.remove('tabs-link-selected'));
 
     // Add a class named "tabs-link-selected" to this link
     // this.element;
